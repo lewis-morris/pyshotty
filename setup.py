7f5a03d339194540791ac2d513bf92c3e64df2ad
lewis <lewis.morris@gmail.com>
2021-06-25 11:22:16 +0100

    change to non subprocess

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pyshotty',
-    version='0.0.2',
+    version='0.0.3',
     packages=['pyshotty'],
     url='https://github.com/lewis-morris/pyshotty',
     license='MIT',
